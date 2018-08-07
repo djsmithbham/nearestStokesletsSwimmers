629094c059b303507d8e807378ccd29c47b86be7
Meurig Gallagher <m.t.gallagher@bham.ac.uk>
2018-08-07 13:51:52 +0100

    Clean up of files

--- dpolyval.m
+++
@@ -1,7 +0,0 @@
-function dy=dpolyval(p,x)
-
-% evaluates first derivative of polynomial with coefficients in descending
-% order in p, at x
-p=p(:)';
-q=p(1:end-1).*(length(p)-1:-1:1);
-dy=polyval(q,x);
